dc5091faaf60c321a4c73212f8c84742d126209c
alex <verba.20@bk.ru>
2021-02-24 18:14:28 +0300

    debbug-one

--- src/assets/js/components/cake.js
+++ src/assets/js/components/cake.js
@@ -4,14 +4,30 @@ items.forEach(item => {
     item.addEventListener("mouseover", e => {
         const body = e.target.closest(".cake__item");
         if (body) {
+            let tranClass;
             if (!body.querySelector(".cake__info")) {
+                switch(body.dataset.element) {
+                    case "1": {
+                        tranClass = "cake__goRight";
+                        break;
+                    } 
+                    case "2": {
+                        tranClass = "cake__goCenter";
+                        break;
+                    }
+                    case "3": {
+                        tranClass = "cake__goLeft";
+                        break;
+                    }
+                }
                 const el = document.createElement("div");
-                el.classList.add("cake__info");
+                el.classList.add("cake__info", tranClass);
+                console.log(el);
                 el.innerHTML = `
                 <h3 class="cake__title">${body.dataset.name}</h3>
                 <p class="cake__description">${body.dataset.info}</p>
                 <div class="cake__footer">
-                <span class="cake__gram">${body.dataset.gramm}Г</span>
+                <span class="cake__gram">${body.dataset.gramm}г.</span>
                 <span class="cake__price">${body.dataset.price}Р</span>
                 `;
                 body.append(el);
