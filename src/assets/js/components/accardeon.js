6e1c851050ba39414ed2c57131f767a3e89259f7
alex <verba.20@bk.ru>
2021-02-24 12:43:04 +0300

    end

--- src/assets/js/components/accardeon.js
+++ src/assets/js/components/accardeon.js
@@ -1,11 +1,13 @@
-const btn = document.querySelector(".menu__menu")
-const block = document.querySelector(".menu__list");
+try {
+    const btn = document.querySelector(".menu__menu")
+    const block = document.querySelector(".menu__list");
 
-btn.addEventListener("click", () => {
-    btn.classList.toggle("menu__menu-active");
-    if (btn.classList.contains("menu__menu-active")) {
-        block.style.display = "block";
-    } else {
-        block.style.display = "none";
-    }
-});
+    btn.addEventListener("click", () => {
+        btn.classList.toggle("menu__menu-active");
+        if (btn.classList.contains("menu__menu-active")) {
+            block.style.display = "block";
+        } else {
+            block.style.display = "none";
+        }
+    });
+} catch {}
